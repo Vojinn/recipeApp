c97b5c0f678c4269992edf42792bc5dc16994346
Vojin <dvojinovic38@gmail.com>
2023-03-19 17:59:22 +0100

    Init commit

---
+++ RecipeSaver/RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Dusan Vojinovic on 19.3.23..
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
